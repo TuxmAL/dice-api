9b879543ca9391414331cb3aa176f1f51ec67fab
Antonio Liccardo <tuxmal@tiscali.it>
2017-01-14 22:17:43 +0100

    Some bug correction an renaming of params and vars.

--- lib/dice_api.rb
+++ lib/dice_api.rb
@@ -9,34 +9,39 @@ dice = Dice.new
 ###### Sinatra Part ###### 
 
 #set :port, 8080 
-#set :environment, :production 
+#set :environment, :production
 
-get '/lancia/:num_dadi' do
-  p params.inspect
+after do
+    response['Access-Control-Allow-Origin']="*" unless settings.production?
+end
+
+get '/lancia/:num_dice' do
   return_message = {}
-  if params.has_key?('num_dadi') then
-    num_dadi = (params['num_dadi']).to_i
-    return return_message[:status] = 'Errore - Il numero di dadi non è valido!' if num_dadi < 1 
-  end  
-  num_dadi ||= 1  
-  if params.has_key?('facce') then
-    facce = (params['facce']).to_i
-    return return_message[:status] = 'Errore - Il numero di facce non è valido!' if facce < 2 
-  end
-  facce ||= 2
-  
-  r = dice.roll!(num_dadi, facce)
   return_message[:status] = 'Ok' 
-  return_message[:dadi] = num_dadi
-  return_message[:facce] = facce
-  return_message[:tiro] = r
-  return_message[:valore] = r.inject(:+)
-  return_message.to_json 
+  if params.has_key?('num_dice') then
+    num_dice = (params['num_dice']).to_i
+    return_message[:status] = 'Error - The number of dice is not valid!' if num_dice < 1 
+  end  
+  num_dice ||= 1  
+  if params.has_key?('faces') then
+    faces = (params['faces']).to_i
+    return_message[:status] = 'Error - The numer of faces is not valid!' if faces < 2 
+  end
+  faces ||= 2
+  
+  if (return_message[:status] == 'Ok') then
+    r = dice.roll!(num_dice, faces)
+    return_message[:dice] = num_dice
+    return_message[:faces] = faces
+    return_message[:throw] = r
+    return_message[:value] = r.inject(:+)
+  end
+  return_message.to_json
 end 
 
-post '/usa/:facce' do
+post '/usa/:faces' do
   return_message = {} 
-  facce = JSON.parse(params[:facce],:symbolize_names => true) 
+  faces = JSON.parse(params[:faces],:symbolize_names => true) 
   
 end
 
