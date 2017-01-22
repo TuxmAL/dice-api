c9be2a05d82349c448112fe3a2324a0d3c1b845b
Antonio Liccardo <tuxmal@tiscali.it>
2017-01-22 20:58:55 +0100

    Added some constraints on in input value: dice numbers and face numbers.

--- lib/dice_api.rb
+++ lib/dice_api.rb
@@ -20,12 +20,14 @@ get '/lancia/:num_dice' do
   return_message[:status] = 'Ok'
   if params.has_key?('num_dice') then
     num_dice = (params['num_dice']).to_i
-    return_message[:status] = 'Error - The number of dice is not valid!' if num_dice < 1 
-  end  
-  num_dice ||= 1  
+    return_message[:status] = 'Error - The number of dice is not valid!' if num_dice < 1
+    return_message[:status] = 'Error - too much dice to throw!' if num_dice > 100
+  end
+  num_dice ||= 1
   if params.has_key?('faces') then
     faces = (params['faces']).to_i
-    return_message[:status] = 'Error - The numer of faces is not valid!' if faces < 2 
+    return_message[:status] = 'Error - The numer of faces is not valid!' if faces < 2
+    return_message[:status] = 'Error - too much faces in a dice!' if faces > 65535
   end
   faces ||= 2
 
