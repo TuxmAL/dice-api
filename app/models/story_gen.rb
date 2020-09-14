f6c53cda7531d99a069c375cdbebc03f81401b88
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2020-09-14 19:11:31 +0200

    Added a story generator using Tracerty gem.

---
+++ app/models/story_gen.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# story_gen.rb
+require 'tracery'
+
+class StoryGen
+  include Tracery
+
+  def initialize
+    story = {
+      'nome': ['Linus Torvalds', 'Matz Yukihiro Matsumoto', 'Anders Hejlsberg', 'Dennis Ritchie',
+               'Steve Wozniak', 'Guido van Rossum', 'James Gosling', 'Bjärne Stroustrup',
+               'John McCarthy', 'Brendan Eich', 'Alan Kay', 'Alain Colmerauer'],
+      'prodotto': ['Linux', 'Java', 'Ruby', 'C#', 'Python', 'C++', 'C', 'AppleDOS',
+                   'Lisp', 'Javascript', 'Smalltalk', 'Prolog'],
+      'umore': %w[indispettito indignato appassionato malinconico astuto cortese paranoico accalorato glaciale
+                  distaccato imperturbabile agghiacciato entusiasta],
+      'motivation': ['semplice', 'complicato', 'incasinato', 'inusabile', 'teorico', 'di basso livello', 'generico',
+                     'duttile', 'difficile', 'illegibile', 'imperativo', 'orientato ai messaggi'],
+      'avversativa': %w[ma tuttavia sebbebe nonostante eppure invece nondimeno],
+      'aspettativa': %w[contento felice arrabbiato critico deluso sorpreso disinteressato indifferente sbalordito
+                        meravigliato],
+      'storia': ['#eroe#, #umore#, creò #prod#, perché #prodotto# era troppo #motivation#. #avversativa# #nome# non ne fu #aspettativa#.'],
+      'origine': ['#[eroe:#nome#][prod:#prodotto#]storia#']
+    }
+    @grammar = createGrammar(story)
+    @grammar.clearState
+  end
+
+  def generate
+    @grammar.flatten('#origine#')
+    # all_errors = root.allErrors
+    # root.finishedText
+  end
+end
