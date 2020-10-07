6ca16e13f3fe73095371bcfc52180abda4e7b15c
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2020-10-07 12:38:30 +0200

    The new Capistrano warns about unrecognized settings now removed:    :copy_exclude, :deploy_via, :shared_children, :use_sudo.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,13 +3,9 @@
 # config valid only for current version of Capistrano
 lock '3.11.2'
 set :application, 'dice-api'
-set :use_sudo, true
 set :repo_url, "tony@#{my_first_private_ipv4.ip_address}:/home/tony/Progetti/dice-api" # path to local git repository
 set :branch, 'master'
 set :deploy_to, '/var/www/ruby/dice-api'
-set :deploy_via, :copy
-set :copy_exclude, ['.git', '.gitignore', 'Capfile', 'Gemfile', '.config', 'config', 'lib', 'resources', 'apache']
-set :shared_children, %w[] # dont create shared folders
 
 namespace :deploy do
   task :start do
