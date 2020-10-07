5a5e917bb015113fc59e592563e119a109b55b7c
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2020-10-07 14:22:23 +0200

    Added Story Generator route to expose new stories.

--- app/app.rb
+++ app/app.rb
@@ -9,11 +9,13 @@ require_relative 'models/dice'
 require_relative 'models/doah_deck'
 require_relative 'models/bofh_gen'
 require_relative 'models/hacker_says'
+require_relative 'models/story_gen'
 
 set :dice, Dice.new
 set :deck, DevOpsAgainstHumanityDeck.new
 set :bofh_gen, BastardOperatorsFromHellGen.new
 set :hacker_says, HackerSays.new
+set :story_gen, StoryGen.new
 
 after do
   response['Access-Control-Allow-Origin']="*" unless settings.production?
@@ -23,6 +25,7 @@ require_relative 'routes/dice'
 require_relative 'routes/doah'
 require_relative 'routes/bofh_gen'
 require_relative 'routes/hckr_says'
+require_relative 'routes/story_gen'
 
 #set :port, 9292 
 set :environment, :production
---
+++ app/routes/story_gen.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+# encoding: utf-8
+# story_gen.rb
+
+get '/genera' do
+  return_message = {}
+  return_message[:value] = settings.story_gen.generate
+  return_message.to_json
+end
