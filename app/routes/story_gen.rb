c75848d0772b14170c357c6aa10f4e6ffdd5db16
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2020-10-13 09:47:42 +0200

    Generate stories in English language.

--- app/routes/story_gen.rb
+++ app/routes/story_gen.rb
@@ -7,3 +7,9 @@ get '/racconta' do
   return_message[:value] = settings.story_gen.generate
   return_message.to_json
 end
+
+get '/tell' do
+  return_message = {}
+  return_message[:value] = settings.story_gen_en.generate
+  return_message.to_json
+end
